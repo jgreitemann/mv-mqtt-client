984dfee74c31046f82f06e1879bda2f5ee502b37
Jonas Greitemann <Jonas.Greitemann@mvtec.com>
2021-08-26 12:00:22 +0200

    Enable calling Halt & Reset in the `Error` state

--- src/mvjson/helpers.rs
+++ src/mvjson/helpers.rs
@@ -14,6 +14,7 @@ pub fn available_actions(state: State) -> EnumMap<ActionType, bool> {
             _ => false
         },
         State::Error => enum_map! {
+            ActionType::Reset | ActionType::Halt => true,
             _ => false
         },
         State::Initialized => enum_map! {
